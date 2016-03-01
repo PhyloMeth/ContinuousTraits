9b628d37590400ca9093137b80dca0d4a28c2148
Brian OMeara <omeara.brian@gmail.com>
2016-03-01 11:26:09 -0500

    ace for mapping states

--- ContinuousBatch.R
+++ ContinuousBatch.R
@@ -35,6 +35,19 @@ delta.AIC.OU1 <- ________FIGURE_OUT_HOW_TO_DO_THIS_____
 #performing a thorough numerical search. It took you 3+ years
 #to get the data, may as well take an extra five minutes to 
 #get an accurate answer
+
+#First, we need to assign regimes. The way we do this is with ancestral state estimation of a discrete trait.
+#We can do this using ace() in ape, or similar functions in corHMM or diversitree. Use only one discrete char
+one.discrete.char <- _____________
+reconstruction.info <- ace(one.discrete.char, tree, type="discrete", method="ML", CI=FALSE)
+best.states <- colnames(reconstruction.info)[apply(reconstruction.info$lik.anc, 1, which.max)]
+
+
+#NOW ADD THESE AS NODE LABELS TO YOUR TREE
+
+labeled.tree <- ________________
+
+
 nodeBased.OUMV <- OUwie(tree, cleaned.continuous,model="OUMV", simmap.tree=FALSE, diagn=FALSE)
 print(nodeBased.OUMV)
 #What do the numbers mean?
@@ -43,3 +56,85 @@ print(nodeBased.OUMV)
 models <- c("BM1","BMS","OU1","OUM","OUMV","OUMA","OUMVA")
 results <- lapply(models, RunSingleOUwieModel, phy=tree, data=trait)
 
+AICc.values<-sapply(results, "[[", "AICc")
+names(AICc.values)<-models
+AICc.values<-AICc.values-min(AICc.values)
+
+
+print(AICc.values) #The best model is the one with smallest AICc score
+
+best<-results[[which.min(AICc.values)]] #store for later
+
+print(best) #prints info on best model
+
+
+#We get SE for the optima (see nodeBased.OUMV$theta) but not for the other parameters. Let's see how hard they are to estimate. 
+#First, look at ?OUwie.fixed to see how to calculate likelihood at a single point.
+?OUwie.fixed
+
+#Next, keep all parameters but alpha at their maximum likelihood estimates (better would be to fix just alpha and let the others optimize given this constraint, but this is harder to program for this class). Try a range of alpha values and plot the likelihood against this.
+alpha.values<-seq(from= _______________ , to= _______________ , length.out=50)
+stop("replace the _______________ and delete this stop")
+
+#keep it simple (and slow) and do a for loop:
+likelihood.values <- rep(NA, length(alpha.values))
+for (iteration in sequence(length(alpha.values))) {
+	likelihood.values[iteration] <- OUwie.fixed(tree, trait, model="OUMV", alpha=rep(alpha.values[iteration],2), sigma.sq=best$solution[2,], theta=best$theta[,1])$loglik
+}
+
+plot(x= _______________ , y= _______________, xlab="_______________", ylab="_______________", type="l", bty="n")
+stop("replace the _______________ and delete this stop")
+
+
+points(x=best$solution[1,1], y=best$loglik, pch=16, col="red")
+text(x=best$solution[1,1], y=best$loglik, "unconstrained best", pos=4, col="red")
+
+#a rule of thumb for confidence for likelihood is all points two log likelihood units worse than the best value. Draw a dotted line on the plot to show this
+abline(h=_______________, lty="dotted") #Two log-likelihood 
+stop("replace the _______________ and delete this stop")
+
+
+#Now, let's try looking at both theta parameters at once, keeping the other parameters at their MLEs
+require("akima")
+
+nreps<-400
+theta1.points<-c(best$theta[1,1], rnorm(nreps-1, best$theta[1,1], 5*best$theta[1,2])) #center on optimal value, have extra variance
+theta2.points<-c(best$theta[2,1], rnorm(nreps-1, best$theta[2,1], 5*best$theta[2,2])) #center on optimal value, have extra variance
+likelihood.values<-rep(NA,nreps)
+
+for (iteration in sequence(nreps)) {
+	likelihood.values[iteration] <- OUwie.fixed(tree, trait, model="OUMV", alpha=best$solution[1,], sigma.sq=best$solution[2,], theta=c(theta1.points[iteration], theta2.points[iteration]))$loglik
+}
+#think of how long that took to do 400 iterations. Now remember how long the search took (longer).
+
+likelihood.differences<-(-(likelihood.values-max(likelihood.values)))
+
+#We are interpolating here: contour wants a nice grid. But by centering our simulations on the MLE values, we made sure to sample most thoroughly there
+interpolated.points<-interp(x=theta1.points, y=theta2.points, z= likelihood.differences, linear=FALSE, extrap=TRUE, xo=seq(min(theta1.points), max(theta1.points), length = 400), yo=seq(min(theta2.points), max(theta2.points), length = 400))
+	
+contour(interpolated.points, xlim=range(c(theta1.points, theta2.points)),ylim=range(c(theta1.points, theta2.points)), xlab="Theta 1", ylab="Theta 2", levels=c(2,5,10),add=FALSE,lwd=1, bty="n", asp=1)
+
+points(x=best$theta[1,1], y=best$theta[2,1], col="red", pch=16)
+
+points(x=trait$X[which(trait$Reg==1)],y=rep(min(c(theta1.points, theta2.points)), length(which(trait$Reg==1))), pch=18, col=rgb(0,0,0,.3)) #the tip values in regime 1, plotted along x axis
+points(y=trait$X[which(trait$Reg==2)],x=rep(min(c(theta1.points, theta2.points)), length(which(trait$Reg==2))), pch=18, col=rgb(0,0,0,.3)) #the tip values in regime 2, plotted along y axis
+
+
+#The below only works if the discrete trait rate is low, so you have a good chance of estimating where the state is.
+#If it evolves quickly, hard to estimate where the regimes are, so some in regime 1 are incorrectly mapped in
+#regime 2 vice versa. This makes the models more similar than they should be.
+#See Revell 2013, DOI:10.1093/sysbio/sys084 for an exploration of this effect.
+library(phytools)
+trait.ordered<-data.frame(trait[,2], trait[,2],row.names=trait[,1])
+trait.ordered<- trait.ordered[tree$tip.label,]
+z<-trait.ordered[,1]
+names(z)<-rownames(trait.ordered)
+tree.mapped<-make.simmap(tree,z,model="ER",nsim=1)
+leg<-c("black","red")
+names(leg)<-c(1,2)
+plotSimmap(tree.mapped,leg,pts=FALSE,ftype="off", lwd=1)
+
+simmapBased<-OUwie(tree.mapped,trait,model="OUMV", simmap.tree=TRUE, diagn=FALSE)
+print(simmapBased)
+#How does this compare to our best model from above? Should they be directly comparable?
+print(best)
